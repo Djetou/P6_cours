329709677c7d60ae0339b9879276d3eddb7c1b75
OUSSENI <98464297+Djetou@users.noreply.github.com>
2022-10-31 11:22:57 +0100

    Add files via upload

---
+++ back/models/Thing.js
@@ -0,0 +1,11 @@
+const mongoose = require ('mangoose');
+
+const thingSchema = mangoose.Schema({
+    title: {type: String, required: true },
+    description: {type: String, required: true} ,
+    imageUrl: { type: String, required: true },
+    userId: { type: String, required: true },
+    price: { type: Number, required: true },
+});
+
+module.exports = mangoose.model('Thing', thingSchema);
